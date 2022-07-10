f4bed9b30b0ce8e05d996bdfc0058412901d8e64
Arnaud MAZEL <arnaud.mazel@epitech.eu>
2022-07-10 18:01:44 +0200

    wip: added search input,
    where did the search route go ?

--- src/components/sidebar/SidebarHeader.vue
+++ src/components/sidebar/SidebarHeader.vue
@@ -5,23 +5,38 @@ export default {
   name: 'SidebarHeader',
 
   components: { BackspaceIcon },
+
+  props: {
+    modelValue: String,
+  },
+
+  emits: [
+    'update:modelValue',
+    'search',
+  ],
 };
 </script>
 
 <template>
   <div class="flex flex-row">
     <div class="w-full">
-      <input class="w-full">
+      <input
+          :value="modelValue"
+          @input="$emit('update:modelValue', $event.target.value)"
+          class="w-full"
+      >
     </div>
 
-    <div>
-      <button>
+    <div v-show="modelValue.length > 0">
+      <button @click="$emit('update:modelValue', '')">
         <BackspaceIcon class="h-6 w-6 text-black"/>
       </button>
     </div>
 
     <div>
-      <button>Search</button>
+      <button @click="$emit('search')">
+        Search
+      </button>
     </div>
   </div>
 </template>
--- src/components/sidebar/TheSidebar.vue
+++ src/components/sidebar/TheSidebar.vue
@@ -1,4 +1,6 @@
 <script>
+import { mapStores } from 'pinia';
+import { useEsiStore } from '@/stores/esi';
 import SidebarHeader from '@/components/sidebar/SidebarHeader'
 import MarketBrowser from '@/components/sidebar/browser/MarketBrowser'
 import MarketSearch from '@/components/sidebar/browser/MarketSearch';
@@ -11,12 +13,45 @@ export default {
     MarketBrowser,
     MarketSearch,
   },
+
+  data() {
+    return {
+      searchString: '',
+      searchResult: [],
+    };
+  },
+
+  computed: {
+    ...mapStores(useEsiStore),
+  },
+
+  watch: {
+    searchString() {
+      if (this.searchString === '') {
+        this.searchResult = [];
+      }
+    }
+  },
+
+  methods: {
+    handleSearch() {
+      this.esiStore.searchItem(this.searchString)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        });
+    }
+  },
 }
 </script>
 
 <template>
   <nav>
-    <SidebarHeader/>
+    <SidebarHeader
+        :modelValue="searchString"
+        @update:modelValue="newValue => searchString = newValue"
+        @search="handleSearch"
+    />
 
     <MarketBrowser/>
 
