8cb79e992dd84522ba9577e4100d6e0e1e3f7eef
Arnaud MAZEL <arnaud.mazel@epitech.eu>
2022-07-23 20:49:39 +0200

    feat: added MA, Dochian and min/max to chart

--- src/components/content/details/history/HistoryGraph.vue
+++ src/components/content/details/history/HistoryGraph.vue
@@ -18,6 +18,10 @@ export default {
     return {
       chartData: {},
       chartOptions: {
+        interaction: {
+          intersect: false,
+          mode: 'index',
+        },
         scales: {
           priceScale: {
             type: 'linear',
@@ -40,19 +44,53 @@ export default {
         .slice(-this.period)
         .map(stat => stat.date);
     },
-    dataAverage() {
+    dataMovingAverage5() {
+      return this.computeMovingAverage(5);
+    },
+    dataMovingAverage20() {
+      return this.computeMovingAverage(20);
+    },
+    dataMedian() {
       return this.statistics
         .slice(-this.period)
         .map(stat => stat.average);
+    },
+    dataDonchianTop() {
+      const highs = this.statistics
+        .slice(-this.period)
+        .map(stat => stat.highest);
+      return highs.map((element, index, array) => {
+        const target = index - 5;
+        const start = Math.max(0, target);
+        const subArray = array.slice(start, index);
+        return Math.max(element, ...subArray);
+      });
+    },
+    dataDonchianBot() {
+      const lows = this.statistics
+        .slice(-this.period)
+        .map(stat => stat.lowest);
+      return lows.map((element, index, array) => {
+        const target = index - 5;
+        const start = Math.max(0, target);
+        const subArray = array.slice(start, index);
+        return Math.min(element, ...subArray);
+      });
+    },
+    dataMax() {
+      return this.statistics
+        .slice(-this.period)
+        .map(stat => stat.highest);
+    },
+    dataMin() {
+      return this.statistics
+        .slice(-this.period)
+        .map(stat => stat.lowest);
     },
     dataVolume() {
       return this.statistics
         .slice(-this.period)
         .map(stat => stat.volume);
-    },
-    dataDonchianTop() {
-    },
-    dataDonchianBot() {
     },
   },
 
@@ -70,14 +108,72 @@ export default {
   },
 
   methods: {
+    computeMovingAverage(duration) {
+      const averages = this.statistics
+        .slice(-this.period)
+        .map(stat => stat.average);
+      return averages.map((element, index, array) => {
+        const target = index - duration;
+        const start = Math.max(0, target);
+        const subArray = array.slice(start, index + 1);
+        const sum = subArray.reduce((previous, current) => previous + current, 0);
+        return index !== 0
+          ? sum / subArray.length
+          : element;
+      });
+    },
+
     updateChart() {
       this.chartData = {
         labels: this.dataLabel,
         datasets: [
           {
             label: 'Median',
-            data: this.dataAverage,
-            backgroundColor: 'rgba(255,191,0,1.0)',
+            data: this.dataMedian,
+            backgroundColor: 'rgba(255, 191, 0)',
+            showLine: false,
+            yAxisID: 'priceScale',
+          },
+          {
+            label: 'Moving Average 5d',
+            data: this.dataMovingAverage5,
+            borderColor: 'rgb(0,0,255)',
+            backgroundColor: 'rgb(0,0,255)',
+            yAxisID: 'priceScale',
+          },
+          {
+            label: 'Moving Average 20d',
+            data: this.dataMovingAverage20,
+            borderColor: 'rgb(255,0,0)',
+            backgroundColor: 'rgb(255,0,0)',
+            yAxisID: 'priceScale',
+          },
+          {
+            label: 'Donchian channel top',
+            data: this.dataDonchianTop,
+            backgroundColor: 'rgb(191, 191, 191, 0.2)',
+            showLine: false,
+            yAxisID: 'priceScale',
+            fill: '+1',
+          },
+          {
+            label: 'Donchian channel bot',
+            data: this.dataDonchianBot,
+            backgroundColor: 'rgb(191, 191, 191, 0.2)',
+            showLine: false,
+            yAxisID: 'priceScale',
+          },
+          {
+            label: 'Max',
+            data: this.dataMax,
+            backgroundColor: 'rgb(127, 127, 127)',
+            showLine: false,
+            yAxisID: 'priceScale',
+          },
+          {
+            label: 'Min',
+            data: this.dataMin,
+            backgroundColor: 'rgb(127, 127, 127)',
             showLine: false,
             yAxisID: 'priceScale',
           },
@@ -85,7 +181,7 @@ export default {
             type: 'bar',
             label: 'Volume',
             data: this.dataVolume,
-            backgroundColor: 'rgb(0,191,255,1.0)',
+            backgroundColor: 'rgb(0,191,255)',
             yAxisID: 'volumeScale',
           },
         ],
